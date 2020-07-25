9b126f93dd644c87ea82bfef4f46f0ad9b7bcacf
Valentins Fedotovs <vtrader@inbox.lv>
2020-07-25 21:35:17 +0100

    Refactoring removed function from my_modules

--- my_modules.py
+++ my_modules.py
@@ -20,51 +20,6 @@ def shuffle_pass(str_list: list) -> str:
     return pass_char_list
 
 
-def collect_pwd_reqs() -> list:
-    "Function collects password requiremnts from STDIN"
-    "Function verifies that correct/valid values are entered"
-    valid_coices = ['Y', 'N', 'y', 'n']
-    min_len = 4
-    max_len = 30
-    req_list = []
-    len_valid = True
-    format_valid = True
-
-    while format_valid:
-        try:
-            pwd_len = int(input("Choose password lenght(characters): "))
-            format_valid = False
-        except ValueError:
-            print("Password lenght should be digit")
-
-    while len_valid:
-        if pwd_len > min_len and pwd_len < max_len:
-            req_list.append(pwd_len)
-            len_valid = False
-        if pwd_len < min_len:
-            print("Minimum password lenght is 4, try again")
-            pwd_len = int(input("Choose password lenght(characters): "))
-
-        if pwd_len > max_len:
-            print("Max password lenght is 30 characters, try again")
-            pwd_len = int(input("Choose password lenght(characters): "))
-
-    include_letter_up = str(
-        input("Do you want include upper case letters (y/n)?:"))
-    include_letter_low = str(
-        input("Do you want include lower case letters (y/n)?:"))
-    include_nums = str(input("Do you want include numbers (y/n)?:"))
-    include_specials = str(
-        input("Do you want include special characters (y/n)?:"))
-
-    req_list.append(include_letter_up)
-    req_list.append(include_letter_low)
-    req_list.append(include_nums)
-    req_list.append(include_specials)
-
-    return req_list
-
-
 def get_pwd_len() -> int:
     "Function collects password requiremnts from STDIN and "
     "validates that value is int and > 4 and < 30"
